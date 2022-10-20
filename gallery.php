a1d5799f091e45ddacc34b7bde9eb60d5c0c3d9c
camilabogner <112421758+camilabogner@users.noreply.github.com>
2022-10-20 01:01:31 +0100

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Gallery</title>
+<meta charset="UTC+1">
+ <meta name="viewport" content="width=device-width,initial-scale=1.0">   
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="script/gallery.js"></script>
+
+</head>
+ 
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Photos 4Fun By Carol Bogner</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="gallery.php">Gallery <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+        
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+        
+      </li>
+      <li class="nav-item dropdown">
+       
+      
+    </ul>
+    
+  </div>
+</nav>
+
+<section class="my-4">
+<div class="py-4">
+    <div class="container-fluid">
+    <div class="col-lg-4 col-md-4 col-12"></div>
+    <div class="row">
+
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/IMG_0353.jpg" class="img-fluid" pb-3>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/IMG_2388.jpg" class="img-fluid" pb-3>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/IMG_9213.jpg" class="img-fluid" pb-3>
+</div>
+
+</section>
+
+<section class="my-4">
+<div class="py-4">
+  
+  <div class="container-fluid">
+    <div class="row">
+
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/nature3.jpg" class="img-fluid" pb-3>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/IMG_0729-2.jpg" class="img-fluid" pb-3>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/nature4.jpg" class="img-fluid" pb-3>
+</div>
+
+</section>
+
+<section class="my-4">
+<div class="py-4">
+      <div class="row">
+
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/IMG_2205.jpg" class="img-fluid" pb-3>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/IMG_2119.jpg" class="img-fluid" pb-3>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/revelar1.jpg" class="img-fluid" pb-3>
+</div>
+
+</section>
+ 
+<section class="my-4">
+<div class="py-4">
+  
+  <div class="container-fluid">
+    <div class="row">
+
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/people7.jpg" class="img-fluid" pb-3>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/nature6.jpg" class="img-fluid" pb-3>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+  <img src="images/animals5.jpg" class="img-fluid" pb-3>
+</div>
+
+</section>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/und/popper,min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</script>
+<footer>&copy; Copyright 2022 Photos4Fun By Carol Bogner all rights reserved</footer>
+</body>
+</html>
